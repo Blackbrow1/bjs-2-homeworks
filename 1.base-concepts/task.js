3ae311dead293dc7020ee29d359dc55aa29ee797
Александр Чернобров <alesandro83@mail.ru>
2025-11-09 16:46:56 +0300

    Выполнил задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  const discriminant = b ** 2 - 4 * a * c;
+  
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    const sqrtD = Math.sqrt(discriminant);
+    const root1 = (-b + sqrtD) / (2 * a);
+    const root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+  }
   
   return arr;
 }
